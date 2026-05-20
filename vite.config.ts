ac2f4dc04b7bd0c85233228e8911c1ae3f5cf9e3
Vinicius Rossi <viniciusprossi18@gmail.com>
2026-05-20 15:52:29 -0300

    repository rename

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@ import react from '@vitejs/plugin-react';
 import tailwindcss from '@tailwindcss/vite';
 import { defineConfig } from 'vite';
 
-const GITHUB_PAGES_BASE = '/RickAndMortyPortal/';
+const GITHUB_PAGES_BASE = '/rick-and-morty-portal/';
 
 export default defineConfig({
    base: process.env.NODE_ENV === 'production' ? GITHUB_PAGES_BASE : '/',
